b3d4ca3654a91c4ecdccee4bacfb10b7091b7f08
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-04-01 12:48:26 -0300

    refactor: reorganize header in pages

--- routes/index.js
+++ routes/index.js
@@ -27,8 +27,9 @@ router.get('/', function(req, res, next) {
 router.get('/contacts', function(req,res, next){
 
   res.render('contacts',{
-    title: 'Contato - Restaurante  Saboroso!' 
-
+    title: 'Contato - Restaurante  Saboroso!' ,
+    background: 'images/img_bg_3.jpg',
+    h1: 'Diga um oi!'
 
   });
 
@@ -38,7 +39,9 @@ router.get('/menus', function(req,res, next){
 
   res.render('menus', {
     
-    title: 'Menus - Restaurante  Saboroso!' 
+    title: 'Menus - Restaurante  Saboroso!' ,
+    background: 'images/img_bg_1.jpg',
+    h1: 'Saboreie nosso menu'
 
 
   });
@@ -49,7 +52,9 @@ router.get('/reservations', function(req,res, next){
 
   res.render('reservations', {
     
-    title: 'Reservas - Restaurante  Saboroso!' 
+    title: 'Reservas - Restaurante  Saboroso!' ,
+    background: 'images/img_bg_2.jpg',
+    h1: 'Reserve uma mesa!'
 
 
   });
@@ -60,7 +65,9 @@ router.get('/services', function(req,res, next){
 
   res.render('services', {
     
-    title: 'Serviços - Restaurante  Saboroso!' 
+    title: 'Serviços - Restaurante  Saboroso!' ,
+    background: 'images/img_bg_1.jpg',
+    h1: 'É um prazer poder servir!'
 
 
   });
