81e92cdf960ff29784b36f9c11485c0b973bbcb4
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-04-02 10:58:16 -0300

    feat: add contact routes and methods to post data in MySQL

--- inc/contacts.js
+++ inc/contacts.js
@@ -2,13 +2,14 @@ var conn = require("./db");
 
 module.exports = {
 
+
     render(req, res, error, success){
 
-        res.render('reservations', {
+        res.render('contacts', {
     
-            title: 'Reservas - Restaurante  Saboroso!' ,
-            background: 'images/img_bg_2.jpg',
-            h1: 'Reserve uma mesa!',
+            title: 'Contacts - Restaurante  Saboroso!' ,
+            background: 'images/img_bg_3.jpg',
+            h1: 'Diga um oi!',
             body: req.body,
             error,
             success
@@ -19,20 +20,15 @@ module.exports = {
 
         return new Promise((resolve,reject)=>{
 
-            let date = fields.date.split('/');
-
-            fields.date = `${date[2]}-${date[1]}-${date[0]}`
-
             conn.query(`
-                INSERT INTO tb_reservations (name,email,people,date,time)
-                VALUES(?, ?, ?, ?, ?)
+                INSERT INTO tb_contacts (name,email,message)
+                VALUES(?, ?, ?)
                 `,
                 [
                     fields.name,
                     fields.email,
-                    fields.people,
-                    fields.date,
-                    fields.time
+                    fields.message
+            
                 ],
                 (err,results)=>{
     
@@ -48,4 +44,5 @@ module.exports = {
         });
     }
 
-};
+
+}
