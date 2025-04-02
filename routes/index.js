f45d2514fa7cbcc4a45ecf14af5c2ad783d661ad
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-04-02 09:46:05 -0300

    refactor: reorganize all includes in pages

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,21 @@
 var conn = require('./../inc/db');
 var express = require('express');
+var menus = require('./../inc/menus')
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  conn.query(`
-    SELECT * FROM tb_menus ORDER BY title
-    `, (err, results)=>{
+  menus.getMenus().then(results=>{
 
-      if(err){
-
-        console.log(err);
-      }
-      res.render('index', { 
-        title: 'Restaurante  Saboroso!' ,
-        menus: results
-      
-      });
-
+    res.render('index', { 
+      title: 'Restaurante  Saboroso!' ,
+      menus: results,
+      isHome: true
+    
     });
+
+  });
 
 });
 
@@ -37,12 +33,20 @@ router.get('/contacts', function(req,res, next){
 
 router.get('/menus', function(req,res, next){
 
-  res.render('menus', {
-    
-    title: 'Menus - Restaurante  Saboroso!' ,
-    background: 'images/img_bg_1.jpg',
-    h1: 'Saboreie nosso menu'
+  menus.getMenus().then(results=>{
 
+    res.render('menus', {
+    
+      title: 'Menus - Restaurante  Saboroso!' ,
+      background: 'images/img_bg_1.jpg',
+      h1: 'Saboreie nosso menu',
+      menus: results,
+      isHome: false
+
+  
+  
+  
+    });
 
   });
 
