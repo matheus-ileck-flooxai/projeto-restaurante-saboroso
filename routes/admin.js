a988b565a99ac0d6fa913451c385712b536fd18f
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-04-02 12:10:32 -0300

    feat: add middleware to validate login session

--- routes/admin.js
+++ routes/admin.js
@@ -1,22 +1,71 @@
 var express = require('express');
+var users = require('./../inc/users')
+
 var router = express.Router();
 
+router.use(function(req,res, next){
+
+
+    if(['/login'].indexOf(req.url) == -1 && !req.session.user){
+
+        res.redirect("/admin/login");
+
+    }
+    else{
+        next();
+    }
+  
+    
+});
+
+router.get("/logout", function(req,res,next){
+
+    delete req.session.user;
+
+    res.redirect("/admin/login")
+
+})
+
+
 router.get('/', function(req, res, next) {
-
-    res.render("admin/index",{
-
-
-    });
+    res.render("admin/index")
 });
 
 router.get('/login', function(req, res, next) {
 
-    if(!req.session.views) req.session.views = 0;
-    
-    
-
-    res.render("admin/login");
+    users.render(req,res,null)
 });
+
+router.post('/login',function(req,res,next){
+
+    if(!req.body.email){
+
+        users.render(req,res,"Preencha o  campo e-mail");
+
+    }
+    else if(!req.body.password){
+
+        users.render(req,res,"Preencha o campo senha");
+    }
+    else{
+
+        users.login(req.body.email, req.body.password).then(user=>{
+
+        req.session.user = user;
+
+        res.redirect("/admin");
+
+        }).catch(err=>{
+
+            if(err){
+                users.render(req,res, err.message || err);
+            }
+        });
+    }
+
+
+});
+
 router.get('/contacts', function(req, res, next) {
 
     res.render("admin/contacts");
