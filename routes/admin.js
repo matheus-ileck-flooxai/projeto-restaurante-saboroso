5f99d45cd2081086ec6b53cc6bb5df1e134606b7
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-04-03 10:04:35 -0300

    feat(menus): insert form data into MySQL

--- routes/admin.js
+++ routes/admin.js
@@ -105,7 +105,6 @@ router.get('/menus', function(req, res, next) {
 
     menus.getMenus().then(data=>{
 
-        console.log("TESTEEEEEEEEEEE", data);
         
 
         res.render("admin/menus", admin.getParams(req,{
@@ -114,6 +113,20 @@ router.get('/menus', function(req, res, next) {
 
     });
 });
+
+router.post('/menus', function(req,res,next){
+
+    menus.save(req.fields, req.files).then(results=>{
+
+        res.send(results);
+
+    }).catch(err=>{
+
+        res.send(err);
+    })
+
+});
+
 router.get('/reservations', function(req, res, next) {
 
     res.render("admin/reservations",admin.getParams(req, {
