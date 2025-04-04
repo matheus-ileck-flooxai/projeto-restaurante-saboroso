9acd3c84b739c0cfe408ec32d1ba3ac830e0722b
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-04-04 09:44:21 -0300

    refactor: simplify form button events on menus

--- public/js/hcode-grid.js
+++ public/js/hcode-grid.js
@@ -103,23 +103,11 @@ class HcodeGrid{
               this.fireEvent('beforeUpdateClick',[e]);
 
               let data = this.getTrData(e);
-
       
               for (let name in data){
       
-                let input = this.formUpdate.querySelector(`[name=${name}]`);
+                this.options.onUpdateLoad(this.formUpdate, name, data);
                 
-                switch(name){
-      
-                  case 'date':
-                  if(input) input.value = moment(data[name]).format('YYYY-MM-DD');
-      
-                  break;
-      
-                  default:
-                  if(input) input.value = data[name];     
-                }
-      
       
               }  
               
